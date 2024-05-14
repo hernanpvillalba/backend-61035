c0319e39f27ac4bc418bff1084a3be9dc66ac8d2
hernan villalba <hernan.p.villalba@outlook.com>
2024-05-14 11:00:19 -0300

    desafio websockets

---
+++ src/public/js/index.js
@@ -0,0 +1,31 @@
+const socketClient = io();
+
+socketClient.on('saludoDesdeElBack', (message)=>{
+    console.log(message);
+
+    socketClient.emit('respuestaDesdeFront', 'Muchas gracias')
+})
+
+const form = document.getElementById('form')
+const inputName = document.getElementById('name')
+const inputPrice = document.getElementById('price')
+const products = document.getElementById('products')
+
+form.onsubmit = (e) =>{
+    e.preventDefault();
+    const name = inputName.value;
+    const price = inputPrice.value;
+    const product = {
+        name, 
+        price
+    };
+    socketClient.emit('newProduct', product);
+}
+
+socketClient.on('products', (arrayProducts)=>{
+    let infoProducts = '';
+    arrayProducts.map((prod) =>{
+        infoProducts += `${prod.name} - $ ${prod.price} </br>`
+    })
+    products.innerHTML = infoProducts
+})
--- src/server.js
+++ src/server.js
@@ -2,13 +2,40 @@ import express from "express";
 import productRouter from "./routes/productRouter.js";
 import cartRouter from "./routes/cartRouter.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import handlebars from 'express-handlebars';
+import { Server } from "socket.io";
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static('./src/public'))
+
 app.use("/api/products", productRouter);
 app.use("/api/cart", cartRouter);
 app.use(errorHandler);
 
-const PORT = 8080;
-app.listen(PORT, () => console.log(`Server OK en el puerto ${PORT}`));
+app.engine('handlebars', handlebars.engine());
+app.set('views', './src/views');
+app.set('view engine', 'handlebars');
+
+app.get('/realtimeproducts', (req, res)=>{
+    res.render('realtimeproducts')
+})
+
+const httpServer = app.listen(8080, () => console.log(`Server OK en el puerto 8080`));
+
+const socketServer = new Server(httpServer)
+
+const products = []
+
+socketServer.on('connection', (socket)=>{
+    console.log(`Usuario conectado: ${socket.id}`);
+    socket.on('disconnect', ()=>{
+        console.log('Usuario desconectado');
+    })
+
+    socket.on('newProduct', (prod)=>{
+        products.push(prod);
+        socketServer.emit('products', products);
+    })
+})
